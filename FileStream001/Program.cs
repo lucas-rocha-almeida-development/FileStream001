3ba8d8d10a0acad1c954cc10adf2ec1d51c92ec1
Lucas Almeida <lucalmr12@gmail.com>
2023-08-04 18:30:46 -0300

    Adicionar arquivos de projeto.

---
+++ FileStream001/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileStream001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Pasta = @"C:\Curso C\Arquivo01.txt";
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                //fs = new FileStream(Pasta,FileMode.Open);
+                
+                sr = File.OpenText(Pasta);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+
+                }
+              
+            }
+            catch (IOException e) {
+                Console.WriteLine("Um erro ocorreu!!" + e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+                  //  if(fs !=null) fs.Close();
+                }
+            }
+        }
+    }
+
